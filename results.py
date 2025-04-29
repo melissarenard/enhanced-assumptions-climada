b7aadafe1bc6448335bd0e3bd62f30c452a973f7
melissarenard <162660703+melissarenard@users.noreply.github.com>
2025-04-29 20:22:01 +1000

    Update results.py and results_plots.ipynb
    Reduce the amount of code needed to produce outputs and plots

--- results.py
+++ results.py
@@ -7,70 +7,87 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import sparse as sp
 
-def get_exceedance_probability_curve(loss_df, plot = True):
-    # Jovana sim_utils
-    loss_per_sim = loss_df.groupby('simulation')['total_loss'].sum()
-    sorted_values = np.sort(loss_per_sim)
-
-    # Calculate exceedance probabilities
-    n = sorted_values.size
-    exceedance_prob = np.sort(np.arange(1,n+1)/n * 100)[::-1]
+def read_losses(
+    homogeneous: bool,
+    n_years: int,
+    loc = False,
+    p_loc = 0.5,
+    intensity = False,
+    damage = False,
+    n_sim = 1_000_000,
+    dir = "Outputs"
+    ) -> np:
     
-    if plot:
-        # Create a frequency-exceedance plot
-        plt.figure(figsize=(8, 6))
-        plt.plot(sorted_values, exceedance_prob, marker='.', linestyle='-')
-        plt.xlabel("Loss")
-        plt.ylabel("Exceedance Probability (%)")
-        plt.title("Aggregate Exceedance Probability Curve")
-        plt.grid()
-        plt.show()
+    loss_df = pd.read_parquet(f'{dir}/{homogeneous}_{n_sim}_{n_years}_{loc}_{p_loc}_{intensity}_{damage}.parquet')
 
-    return exceedance_prob, sorted_values
-
-
-def get_return_period_curve(loss_df, plot = True, rp=250):
     loss_per_sim = loss_df.groupby('simulation')['total_loss'].sum()
-    sorted_values = np.sort(loss_per_sim)
 
-    # Calculate Return Periods
-    n = sorted_values.size
-    exceed_freq = np.linspace(1/n, 1, n)
+    n_sims_gt_0 = n_sim - len(loss_per_sim)
+    loss_per_sim = np.append(loss_per_sim, [0]*n_sims_gt_0)
+
+    return loss_per_sim
+
+def plot_return_period_curves(
+    losses_per_sim: list,
+    legend_labels: dict,
+    n_sim = 1_000_000,
+    rp = 250
+    ):
+    
+    exceed_freq = np.linspace(1/n_sim, 1, n_sim)
     return_period = 1/exceed_freq[::-1]
-    max_idx = int((1-1/rp)*n+1)
+    max_idx = int((1-1/rp)*n_sim + 1)
 
-    if plot:
-        # Create a return-period plot
-        plt.figure(figsize=(7,4))
-        plt.plot(return_period[:max_idx], sorted_values[:max_idx])
-        plt.xlabel("Return Period (years)")
-        plt.ylabel("Loss (USD)")
-        plt.title("Aggregate Exceedance Frequency Curve")
-        plt.grid(color = 'whitesmoke')
-        plt.show()
+    plt.figure(figsize=(7.5, 7.5/1.6))
 
-    return return_period, sorted_values
+    for loss_per_sim in losses_per_sim:
+        sorted_values = np.sort(loss_per_sim)
+
+        for key, value in legend_labels.items():
+            if np.array_equal(loss_per_sim, value):
+                label = key
+        
+        plt.plot(return_period[:max_idx], sorted_values[:max_idx], label=label)
+    
+    plt.xlabel("Return period")
+    plt.ylabel("Loss (USD)")
+    plt.grid(color = 'whitesmoke')
+    plt.legend(loc = "upper left")
+    # plt.xticks(fontsize=12)
+    # plt.yticks(fontsize=12)
+    plt.tight_layout()
+    plt.show()
+
+    return None
 
 
-def risk_measures(loss_df, printing = True):
-    losses = loss_df.groupby('simulation')['total_loss'].sum()
+def risk_measures(
+    losses_per_sim: list,
+    column_labels: dict,
+    units: int
+    ) -> pd:
 
-    quantiles = [25,50,75,95,99,99.5]
+    df = pd.DataFrame()
 
-    quantile_25 = np.percentile(losses, 25)
-    quantile_50 = np.percentile(losses, 50)
-    quantile_75 = np.percentile(losses, 75)
-    quantile_95 = np.percentile(losses, 95)
-    quantile_99 = np.percentile(losses, 99)
-    quantile_995 = np.percentile(losses, 99.5)
-    mean = losses.mean()
-    sd = losses.std()
-    TVaR_99 = np.mean(losses[losses > quantile_99])
-    TVaR_995 = np.mean(losses[losses > quantile_995])
+    for loss_per_sim in losses_per_sim:
+        mean = loss_per_sim.mean()
+        sd = loss_per_sim.std()
+        VaR_50 = np.percentile(loss_per_sim, 50)
+        VaR_75 = np.percentile(loss_per_sim, 75)
+        VaR_95 = np.percentile(loss_per_sim, 95)
+        VaR_99 = np.percentile(loss_per_sim, 99)
+        VaR_995 = np.percentile(loss_per_sim, 99.5)
+        TVaR_99 = np.mean(loss_per_sim[loss_per_sim > VaR_99])
+        TVaR_995 = np.mean(loss_per_sim[loss_per_sim > VaR_995])
 
-    all_measures = [quantile_25, quantile_50, quantile_75, quantile_95, quantile_99, quantile_995, mean, sd, TVaR_99, TVaR_995]
+        all_measures = [VaR_50, VaR_75, VaR_95, VaR_99, VaR_995, mean, sd, TVaR_99, TVaR_995]
 
-    if printing:
-        print(f'Mean Losses: {all_measures[6]:,.0f}\nStandard Deviation Losses: {all_measures[7]:,.0f} \n25% Quantile: {all_measures[0]:,.0f} \n50% Quantile: {all_measures[1]:,.0f} \n75% Quantile: {all_measures[2]:,.0f} \n95% Quantile: {all_measures[3]:,.0f} \n99% Quantile: {all_measures[4]:,.0f} \n99.5% Quantile: {all_measures[5]:,.0f} \n1% TVaR: {all_measures[-2]:,.0f} \n0.5% TVaR: {all_measures[-1]:,.0f}\n')
+        for key, value in column_labels.items():
+            if np.array_equal(loss_per_sim, value):
+                colname = key
 
-    return all_measures
+        df[colname] = all_measures
+
+    df_styled = df.style.format(lambda x: f'{x/units:,.0f}')
+
+    return df_styled
